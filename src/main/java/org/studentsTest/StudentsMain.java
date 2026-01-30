1addb5e06b298f3459958f3001a89fb7aaf1fd26
moootvey <m.slabukhin@gmail.com>
2026-01-30 09:47:01 +0700

    Add main application entry point for Students service

---
+++ src/main/java/org/studentsTest/StudentsMain.java
@@ -0,0 +1,11 @@
+package org.studentsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsMain {
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsMain.class, args);
+    }
+}
